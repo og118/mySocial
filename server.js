2dfd497a4fe30cfdeca35ecf2dc66a41a6785f78
og118 <63297841+og118@users.noreply.github.com>
2020-11-30 09:43:49 +0530

    setting up deployment_9

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ mongoose.connect(DB, {
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    // })
 }
 
 const PORT = process.env.PORT;
