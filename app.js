422c619ef4dec1ffa81300706259abfb99680a86
og118 <63297841+og118@users.noreply.github.com>
2020-11-30 11:34:12 +0530

    setting up deployment_17

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://mys0cial.herokuapp.com'); // add when in dev: http://localhost:3000
+    res.header('Access-Control-Allow-Origin', 'https://mys0cial.herokuapp.com/'); // add when in dev: http://localhost:3000
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
